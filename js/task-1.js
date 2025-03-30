43a720f575d03ffa6a49493756a34db135940743
Oleksandr Polishchuk <aleksice1p@gmail.com>
2025-03-30 19:33:18 +0300

    add functiions

--- js/task-1.js
+++ js/task-1.js
@@ -1,17 +1,18 @@
-function slugify(title) {
-  // переводення у нижній регістр та повернення з ( - )
+function isEnoughCapacity(products, containerSize) {
+  // Обчисслення загальної кількість товарів
+  const totalProducts = Object.values(products).reduce(
+    (sum, quantity) => sum + quantity,
+    0
+  );
 
-  return title.toLowerCase().split(" ").join("-");
+  // Повернення результатів
+  return totalProducts <= containerSize;
 }
+// Перевірка функції
+console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); // true
 
-// Перевірка роботи функції
+console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)); // false
 
-console.log(slugify("Arrays for beginners")); // "arrays-for-beginners"
-console.log(slugify("English for developer")); // "english-for-developer"
-console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
-console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
+console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)); // true
 
-console.log(slugify("Arrays for beginners")); // "arrays-for-beginners"
-console.log(slugify("English for developer")); // "english-for-developer"
-console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
-console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
+console.log(isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)); // false
--- js/task-3.js
+++ js/task-3.js
@@ -1,25 +1,29 @@
-function filterArray(numbers, value) {
-  //  порожній масив
+const profile = {
+  username: "Jacob",
+  playTime: 300,
 
-  const newNumbers = [];
+  // Метод для зміни імені профілю
 
-  // Перевірка всіх елементів масиву
+  changeUsername(newName) {
+    this.username = newName;
+  },
 
-  for (const num of numbers) {
-    if (num > value) {
-      // Додаємо підходяще число у новий масив
+  // Метод для оновлення часу гри
+  updatePlayTime(hours) {
+    this.playTime += hours;
+  },
 
-      newNumbers.push(num);
-    }
-  }
+  // Метод для отримання інформації про профіль
+  getInfo() {
+    return `${this.username} has ${this.playTime} active hours`;
+  },
+};
 
-  return newNumbers; // Повертає новий масив
-}
+// Перевірка функції
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
 
-// Перевірка роботи функції
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
 
-console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
-console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
-console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
-console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
-console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
